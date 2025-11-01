d0b270f0147f74e9fa210713db76e3902ca89f5a
Ricardo Gonzalez <ricgnzlzcr@gmail.com>
2025-11-01 13:35:50 -0400

    code staged for branching

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-print('sup')
+START
+# Given a collection of strings called "strings"
+
+SET empty list called "concatenated"
+ITERATE over each "string" in strings
+    concatenated += string
+RETURN concatenated
